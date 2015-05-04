1ab32f7ddfc39034866d37a69ef5702b9ab8a4c8
David L. Willson <dlwillson@thegeek.nu>
2015-05-03 19:34:43 -0600

    practicing ch. 2

---
+++ marbles.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# What is the diff from that to just /usr/bin/python3?
+# When do I put a docstring vs a shebang line?
+
+# David L. Willson
+
+# The point here is to practice some of the things in Chapter 2 of Dive Into Python 3.
+
+print("I should review the logic stuph.")
+
+a=True
+b=True
+
+c=True
+d=False
+
+e=False
+f=True
+
+g=False
+h=False
+
+print('a =', a, ' b =', b)
+print('c =', c, ' d =', d)
