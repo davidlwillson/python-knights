4217091b6f590397a0a6519dcb0d0cb555d07f44
David L. Willson <DLWillson@TheGeek.NU>
2015-05-16 17:07:25 -0600

    changes from last study

--- marbles.py
+++ marbles.py
@@ -20,5 +20,30 @@ f=True
 g=False
 h=False
 
-print('a =', a, ' b =', b)
+print('a =', a, ' b =', b, 'a * b =', a*b)
 print('c =', c, ' d =', d)
+
+# A Tuple is an unchanging list
+
+rainbow_colors = ('red', 'orange', 'yellow', 'green', 'blue', 'purple')
+
+for col in rainbow_colors:
+    print(col)
+    
+# A List is like an array
+marbles=[0,]
+x = 0
+while x < 47:
+    marbles[x]=rainbow_colors[x % 7]
+
+print(marbles[22])
+
+# A Set is like a bag of unique values
+henry_bag=set(range(1,4))
+print(henry_bag.pop())
+
+# A Dictionary allows key/value storage.
+'''
+marbles[0]['color']='yellow'
+marbles[0]['size']='70mm'
+'''
